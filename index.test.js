710bfbc3102a78bddc58e6729e00f43e6d7af852
Kamil Syrewicz <kamil.syrewicz@gmail.com>
2018-10-19 09:38:44 +0200

    Changed description in index.test.js

--- index.test.js
+++ index.test.js
@@ -4,15 +4,15 @@ import { expect } from 'chai';
 import { range, sum } from '../index';
 
 describe('index.js', () => {
-  it('range function should return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
+  it('Expect range function to return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
     const result = range(1, 10);
     expect(result).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
-  it('range function should return [5, 4, 3, 2]', () => {
+  it('Expect range function to return [5, 4, 3, 2]', () => {
     const result = range(5, 2, -1);
     expect(result).to.eql([5, 4, 3, 2]);
   });
-  it('range function should return an array', () => {
+  it('Expect range function to return an array', () => {
     const result = range(5, 2, -1);
     expect(result).to.be.an.instanceof(Array);
   });
