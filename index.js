e9bb51507db55fda3a03dc806ce4226b1ece0244
Kamil Syrewicz <kamil.syrewicz@gmail.com>
2018-10-19 10:22:25 +0200

    Added index.test.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function reverseArray(elements) {
+const reverseArray = (elements) => {
   const newArray = [];
   const a = elements.length;
   for (let i = 0; i < a; i++) {
     newArray.push(elements[a - i - 1]);
   }
   return newArray;
-}
+};
 const reverseArrayInPlace = (elements) => {
   const elementsCount = elements.length - 1;
-  for (i = 0; i < Math.floor(elements.length / 2); i++) {
+  for (let i = 0; i < Math.floor(elements.length / 2); i++) {
     const a = elements[i];
     elements[i] = elements[elementsCount - i];
     elements[elementsCount - i] = a;
@@ -20,3 +20,5 @@ const arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
+
+export { reverseArray, reverseArrayInPlace };
